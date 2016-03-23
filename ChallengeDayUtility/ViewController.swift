f5bf289821f44b8da3e1763b74512793606c051e
Michelle Beadle <michellebeadle@MacBook-Pro-de-Michelle.local>
2016-03-23 20:09:09 -0300

    testezin de CoreData

--- ChallengeDayUtility/ViewController.swift
+++ ChallengeDayUtility/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var senha: UITextField!
+    @IBOutlet weak var login: UITextField!
+    
+    @IBAction func cadastro(sender: AnyObject) {
+        //fazer isso na página de cadastro - isso salva os dados do usuario em "Usuario" no model
+        var appDel:AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
+        var context: NSManagedObjectContext = appDel.managedObjectContext
+        
+        var NovoUsuario = NSEntityDescription.insertNewObjectForEntityForName("Usuario", inManagedObjectContext: context) as NSManagedObject
+        NovoUsuario.setValue(login.text, forKey: "login")
+        NovoUsuario.setValue(senha.text, forKey: "senha")
+        
+       //try! context.save(nil) //erro, não sei se é preciso mesmo
+        
+        print(NovoUsuario)
+        print("Usuário salvo.")
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let Info = NSBundle.mainBundle().objectForInfoDictionaryKey("InfoClinic") as! String
+        print(Info)
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
